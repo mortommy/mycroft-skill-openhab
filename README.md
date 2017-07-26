0a444d964cf4a12229fd93f0d3fd1c81a05e1e9e
mortommy <tommaso.morello@gmail.com>
2017-07-26 14:49:36 +0200

    v0.7

---
+++ README.md
@@ -0,0 +1,58 @@
+# Openhab skill for Mycroft
+
+This skill adds [Openhab](http://www.openhab.org/) support to [Mycroft](https://mycroft.ai).
+The skill takes advantage of Openhab REST API so it works both with the v1.0 and v2.0 of OH.  
+
+In order to make the oh items public to Mycroft will need to be [tagged](http://docs.openhab.org/addons/io/homekit/readme.html).
+The current version supports only Lighting and Switchable tags and the commands ON and OFF.
+
+Some sample commands for Lighting tagged items are:
+
+```
+- Hey Mycroft, turn on Diningroom Light
+- Hey Mycroft, switch off Kitchen Light
+```
+
+Some sample commands for Switching tagged items are:
+
+```
+- Hey Mycroft, put on Good Night
+```
+
+The items are searched by label, so the items for the above examples could be:
+
+```
+Color DiningroomLight "Diningroom Light" <light> (gKitchen) [ "Lighting" ] {channel="hue:0200:1:bloom1:color"}
+Color KitchenLight "Kitchen Light" <light> (gKitchen) [ "Lighting" ] {channel="hue:0200:1:bloom1:color"}
+```
+```
+Switch 	GoodNight "Good Night"	[ "Switchable" ]	
+```
+
+The Lighting items is suitable for items linked to things since the "send command" REST API is used. The Switchable item uses the "set state" update and is suitable for items not linked to things.
+
+If items are modified in openhab, a refresh in Mycroft is needed by the command:
+
+```
+- Hey Mycroft, refresh the openhab items
+```
+
+## Installation
+
+Clone the repository into your `~/.mycroft/skills` directory. Then install the
+dependencies inside your mycroft virtual environment:
+
+If on picroft just skip the workon part and the directory will be /opt/mycroft/skills
+
+```
+cd ~/.mycroft/skills
+git clone https://github.com/btotharye/mycroft-homeassistant HomeAssistantSkill
+workon mycroft
+cd skill-openhab
+pip install -r requirements.txt
+```
+
+## TODO
+ * add commands for Lighting tagged items: dim, bright
+ * add current item status request
+ * add support for Thermostat tagged items
---
+++ __init__.py
@@ -0,0 +1,179 @@
+# Copyright (c) 2010-2017 by the respective copyright holders.
+
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from fuzzywuzzy import fuzz
+
+import requests
+import json
+
+# v 0.1 - just switch on and switch off a fix light
+# v 0.2 - code review
+# v 0.3 - first working version on fixed light item
+# v 0.4 - getLabeledItems method in order to get all the labeled items from OH
+# v 0.5 - refresh labeled item intent
+# v 0.6 - add findItemName method and import fuzzywuzzy
+# v 0.7 - add intent for switchable items
+
+__author__ = 'mortommy'
+
+LOGGER = getLogger(__name__)
+
+class OpenHabSkill(MycroftSkill):
+	
+	def __init__(self):
+		super(OpenHabSkill, self).__init__(name="OpenHabSkill")
+				
+		self.url = "http://%s:%s/rest" % (self.config.get('host'), self.config.get('port'))
+				
+		self.command_headers = {"Content-type": "text/plain"}
+		
+		self.polling_headers = {"Accept": "application/json"}
+		
+		self.lightingItemsDic = dict()
+		self.switchableItemsDic = dict()
+		self.getLabeledItems()		
+			
+	def initialize(self):
+		self.load_data_files(dirname(__file__))
+		
+		refresh_labeled_items_intent = IntentBuilder("RefreshLabeledItemsIntent").require("RefreshLabeledItemsKeyword").build()
+		self.register_intent(refresh_labeled_items_intent, self.handle_refresh_labeled_items_intent)
+		
+		light_status_intent = IntentBuilder("LightStatusIntent").require("LightStatusKeyword").require("Command").require("Item").build()
+		self.register_intent(light_status_intent, self.handle_light_status_intent)		
+		
+		switch_status_intent = IntentBuilder("SwitchStatusIntent").require("SwitchStatusKeyword").require("Command").require("Item").build()
+		self.register_intent(switch_status_intent, self.handle_switch_status_intent)
+			
+	def getLabeledItems(self):
+		#find all the items labeled Lighting and Switchable from OH
+		#the labeled items are stored in dictionaries
+		
+		self.lightingItemsDic = {}
+		self.switchableItemsDic = {}
+		requestUrl = self.url+"/items?recursive=false"
+				
+		try:
+			req = requests.get(requestUrl, headers=self.polling_headers)
+			if req.status_code == 200:
+				json_response = req.json()
+				for x in range(0,len(json_response)):
+					if ("Lighting" in json_response[x]['tags']):
+						self.lightingItemsDic.update({json_response[x]['name']: json_response[x]['label']})
+					elif ("Switchable" in json_response[x]['tags']):
+						self.switchableItemsDic.update({json_response[x]['name']: json_response[x]['label']})
+					else:
+						pass
+			else:
+				LOGGER.error("Some issues with the command execution!")
+				self.speak_dialog('GetItemsListError')
+				
+		except KeyError:
+					pass
+	
+	def findItemName(self, itemDictionary, messageItem):
+		
+		bestScore = 0
+		score = 0
+		bestItem = None		
+		
+		try:
+			for itemName, itemLabel in itemDictionary.items():
+				score = fuzz.ratio(messageItem, itemLabel)
+				if score > bestScore:
+					bestScore = score
+					bestItem = itemName
+		except KeyError:
+                    pass
+					
+		return bestItem
+	
+	def handle_refresh_labeled_items_intent(self, message):
+		#to refresh the oh items labeled list we use an intent, we can ask Mycroft to make the refresh
+		
+		self.getLabeledItems()
+		dictLenght = str(len(self.lightingItemsDic) + len(self.switchableItemsDic))
+		self.speak_dialog('RefreshLabeledItems', {'number_item': dictLenght})
+	
+	def handle_light_status_intent(self, message):
+		
+		command = message.data["Command"]
+        messageItem = message.data["Item"]
+				
+		#We have to find the item to update from our Lights dictionary
+		ohItem = self.findItemName(self.lightingItemsDic, messageItem)
+		
+		if ohItem != None:
+			if (command != "on") and (command != "off"):
+				self.speak_dialog('ErrorDialog')
+			else:
+				statusCode = self.sendCommandToItem(ohItem, command.upper())
+				if statusCode == 200:
+					if command == "on":
+						self.speak_dialog('LightStatusOn', {'switch_item': messageItem})
+					else:
+						self.speak_dialog('LightStatusOff', {'switch_item': messageItem})
+				elif statusCode == 404:
+					LOGGER.error("Some issues with the command execution!. Item not found")
+					self.speak_dialog('ItemNotFoundError')
+				else:
+					LOGGER.error("Some issues with the command execution!")
+					self.speak_dialog('CommunicationError')
+		else:
+			LOGGER.error("Item not found!")
+			self.speak_dialog('ItemNotFoundError')
+			
+	def handle_switch_status_intent(self, message):
+		
+		command = message.data["Command"]
+		messageItem = message.data["Item"]
+				
+		#We have to find the item to update from our Switchs dictionary
+		ohItem = self.findItemName(self.switchableItemsDic, messageItem)
+		
+		if ohItem != None:
+			if (command != "on") and (command != "off"):
+				self.speak_dialog('ErrorDialog')
+			else:
+				statusCode = self.sendStatusToItem(ohItem, command.upper())
+				if statusCode == 202:
+					if command == "on":
+						self.speak_dialog('SwitchStatusOn', {'switch_item': messageItem})
+					else:
+						self.speak_dialog('SwitchStatusOff', {'switch_item': messageItem})
+				elif statusCode == 404:
+					LOGGER.error("Some issues with the command execution!. Item not found")
+					self.speak_dialog('ItemNotFoundError')
+				else:
+					LOGGER.error("Some issues with the command execution!")
+					self.speak_dialog('CommunicationError')
+		else:
+			LOGGER.error("Item not found!")
+			self.speak_dialog('ItemNotFoundError')
+			
+	def sendStatusToItem(self, ohItem, command):
+		requestUrl = self.url+"/items/%s/state" % (ohItem)
+		req = requests.put(requestUrl, data=command, headers=self.command_headers)
+		
+		return req.status_code
+			
+	def sendCommandToItem(self, ohItem, command):
+		requestUrl = self.url+"/items/%s" % (ohItem)
+		req = requests.post(requestUrl, data=command, headers=self.command_headers)
+		
+		return req.status_code
+		
+	def stop(self):
+		pass
+	 
+def create_skill():
+    return OpenHabSkill()
